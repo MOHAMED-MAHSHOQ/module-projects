ed4473925bbe0793e6e3d1a0bb007e94dd5a5da7
MOHAMED-MAHSHOQ <msmahshoq2005@gmail.com>
2026-04-22 14:50:37 +0530

    changes made

--- library-service/src/main/java/com/university/libraryservice/client/BookServiceClient.java
+++ library-service/src/main/java/com/university/libraryservice/client/BookServiceClient.java
@@ -1,6 +1,7 @@
 package com.university.libraryservice.client;
 
 import com.university.shared.dto.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -9,42 +10,38 @@ import java.util.List;
 
 @Component
 public class BookServiceClient {
-    private static final String BASE_URL = "http://localhost:8082";
-    private static final String BOOKS = "/api/books";
-    private static final String AVAILABLE = "/api/books/available";
-    private static final String SEARCH = "/api/books/search";
-    private static final String BY_ID = "/api/books/{id}";
-    private static final String AVAILABILITY = "/api/books/{id}/available";
+
+    private static final String BOOK_PATH = "/api/books";
+    private static final String BOOK_PATH_ID = "/api/books/{id}";
+
 
     private final RestClient restClient;
 
-    public BookServiceClient(RestClient restClientConfig) {
-        this.restClient = RestClient.builder()
-                .baseUrl(BASE_URL)
-                .build();
+    public BookServiceClient(RestClient restClient) {
+        this.restClient = restClient;
     }
 
 
     public ApiResponse<List<BookSummaryDto>> getAllBooks() {
-        return restClient.get().uri(BOOKS).retrieve()
+        return restClient.get().uri(BOOK_PATH).retrieve()
                 .body(new ParameterizedTypeReference<ApiResponse<List<BookSummaryDto>>>() {
                 });
     }
 
     public ApiResponse<List<BookSummaryDto>> getAvailableBooks() {
-        return restClient.get().uri(AVAILABLE).retrieve()
+        return restClient.get().uri(BOOK_PATH+"/available").retrieve()
                 .body(new ParameterizedTypeReference<ApiResponse<List<BookSummaryDto>>>() {
                 });
     }
 
     public ApiResponse<BookDto> getBookById(Long id) {
-        return restClient.get().uri(BY_ID, id).retrieve()
+        return restClient.get().uri(BOOK_PATH_ID, id).retrieve()
                 .body(new ParameterizedTypeReference<ApiResponse<BookDto>>() {
                 });
     }
 
     public ApiResponse<List<BookSummaryDto>> searchBooks(String title, String author) {
-        String uri = SEARCH + (title != null ? "?title=" + title
+        String uri = BOOK_PATH+"/search" + (title != null ? "?title=" + title
                 : author != null ? "?author=" + author : "");
         return restClient.get().uri(uri).retrieve()
                 .body(new ParameterizedTypeReference<ApiResponse<List<BookSummaryDto>>>() {
@@ -52,7 +49,7 @@ public class BookServiceClient {
     }
 
     public ApiResponse<BookDto> updateAvailability(Long id, boolean available) {
-        return restClient.put().uri(AVAILABILITY + "?available=" + available, id)
+        return restClient.put().uri(BOOK_PATH+"/{id}/available" + "?available=" + available, id)
                 .retrieve().body(new ParameterizedTypeReference<ApiResponse<BookDto>>() {
                 });
     }
--- library-service/src/main/java/com/university/libraryservice/config/RestClientConfig.java
+++ library-service/src/main/java/com/university/libraryservice/config/RestClientConfig.java
@@ -1,13 +1,19 @@
 package com.university.libraryservice.config;
+import com.university.libraryservice.client.BookServiceClient;
 import com.university.shared.auth.BasicAuthHeaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
+    @Value("${book-service.base-url}")
+    private String bookServiceUrl;
+
     @Bean
     public RestClient restClientConfig(){
         return RestClient.builder()
+                .baseUrl(bookServiceUrl)
                 .defaultHeader("Authorization", BasicAuthHeaderBuilder.buildServiceHeader())
                 .defaultHeader("Content-Type", "application/json")
                 .build();
